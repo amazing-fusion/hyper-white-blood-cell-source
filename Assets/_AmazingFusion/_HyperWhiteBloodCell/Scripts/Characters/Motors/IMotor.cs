3564aeed2ce47a9fc20e6f9e0a17c98a6aff05dc
Miguel <mlopez@dreaming-arts.com>
2017-03-01 12:14:46 +0100

    files created

---
+++ Assets/_AmazingFusion/_HyperWhiteBloodCell/Scripts/Characters/Motors/IMotor.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.AmazingFusion.HyperWhiteBloodCell {
+    public interface IMotor {
+
+        void MoveTo(Vector2 destinyPosition);
+    }
+}
