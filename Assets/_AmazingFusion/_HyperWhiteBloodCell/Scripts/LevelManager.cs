0515eaee7e2e7ee82023afff33074ed53fbb3e70
Diego Diaz Perez <diduscdm@gmail.com>
2017-03-01 12:01:04 +0100

    Commit Classes

---
+++ Assets/_AmazingFusion/_HyperWhiteBloodCell/Scripts/LevelManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.AmazingFusion.HyperWhiteBloodCell
+{
+    public class LevelManager : OptimizedBehaviour
+    {
+        
+    }
+}
+
---
+++ Assets/_AmazingFusion/_HyperWhiteBloodCell/Scripts/Room.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.AmazingFusion.HyperWhiteBloodCell
+{
+    public class Room : OptimizedBehaviour
+    {
+        [SerializeField]
+        int _minLevelRequired;
+
+        [SerializeField]
+        int _maxLevelRequired;
+
+        public int MinLevelRequired
+        {
+            get
+            {
+                return _minLevelRequired;
+            }
+
+            set
+            {
+                _minLevelRequired = value;
+            }
+        }
+
+        public int MaxLevelRequired
+        {
+            get
+            {
+                return _maxLevelRequired;
+            }
+
+            set
+            {
+                _maxLevelRequired = value;
+            }
+        }
+
+        public event System.Action<int> OnLevelStart;
+
+        public void StartLevel()
+        {
+
+        }
+
+        public void EndLevel()
+        {
+
+        }
+    }
+}
+
