5556e3476036e5e8638dd461119fcc95a0732758
Miguel <mlopez@dreaming-arts.com>
2017-03-01 11:43:53 +0100

    renames

---
+++ Assets/_AmazingFusion/Tools/Scripts/Effects/Easing/MoveToEasingAnimation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using MovementEffects;
+
+namespace com.AmazingFusion {
+    public class MoveToEasingAnimation : Vector3EasingAnimation {
+
+        public void SetStartPosition(Vector3 startPosition) {
+            _xEasingInfo.StartValue = startPosition.x;
+            _yEasingInfo.StartValue = startPosition.y;
+            _zEasingInfo.StartValue = startPosition.z;
+        }
+
+        public void SetStartPositionAsCurrentLocalPosition() {
+            SetStartPosition(Transform.localPosition);
+        }
+
+        public void SetChangePosition(Vector3 changePosition) {
+            _xEasingInfo.ChangeValue = changePosition.x;
+            _yEasingInfo.ChangeValue = changePosition.y;
+            _zEasingInfo.ChangeValue = changePosition.z;
+        }
+
+        public void SetEndPosition(Vector3 endPosition) {
+            _xEasingInfo.ChangeValue = endPosition.x - (float)_xEasingInfo.StartValue;
+            _yEasingInfo.ChangeValue = endPosition.y - (float)_yEasingInfo.StartValue;
+            _zEasingInfo.ChangeValue = endPosition.z - (float)_zEasingInfo.StartValue;
+        }
+
+        public void SetEndPositionAsCurrentLocalPosition() {
+            SetEndPosition(Transform.localPosition);
+        }
+
+        protected override void EasingUpdate() {
+            base.EasingUpdate();
+            Transform.localPosition = new Vector3(
+                        (float)_xEasingInfo.CurrentValue,
+                        (float)_yEasingInfo.CurrentValue,
+                        (float)_zEasingInfo.CurrentValue);
+        }
+    }
+}
---
+++ Assets/_AmazingFusion/Tools/Scripts/Effects/Easing/ScaleEasingAnimation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace com.AmazingFusion {
+    public class ScaleEasingAnimation : ValueEasingAnimation {
+
+        public void SetStartScaleAsCurrentScale()
+        {
+            _easingInfo.StartValue = Transform.localScale.x;
+        }
+
+
+        protected override void EasingUpdate() {
+            base.EasingUpdate();
+            float scale = (float)_easingInfo.CurrentValue;
+            Transform.localScale = new Vector3(scale, scale, scale);
+        }
+    }
+}
