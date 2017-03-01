eafd0fb146c92f18a5862b34ffdaf32d65cc4e0f
Diego Diaz Perez <diduscdm@gmail.com>
2017-03-01 12:53:00 +0100

    Resolved conflicts

--- Assets/_AmazingFusion/_HyperWhiteBloodCell/Scripts/GameOverView.cs
+++ Assets/_AmazingFusion/_HyperWhiteBloodCell/Scripts/GameOverView.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< HEAD
+
 namespace com.AmazingFusion.HyperWhiteBloodCell
 {
     public class GameOverView : OptimizedBehaviour
@@ -11,17 +11,3 @@ namespace com.AmazingFusion.HyperWhiteBloodCell
     }
 }
 
-=======
-public class GameOverView : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
->>>>>>> 3564aeed2ce47a9fc20e6f9e0a17c98a6aff05dc
--- Assets/_AmazingFusion/_HyperWhiteBloodCell/Scripts/PlayerView.cs
+++ Assets/_AmazingFusion/_HyperWhiteBloodCell/Scripts/PlayerView.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< HEAD
 
 namespace com.AmazingFusion.HyperWhiteBloodCell
 {
@@ -11,18 +10,4 @@ namespace com.AmazingFusion.HyperWhiteBloodCell
 
     }
 }
- 
-=======
-public class PlayerView : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
->>>>>>> 3564aeed2ce47a9fc20e6f9e0a17c98a6aff05dc
