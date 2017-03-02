44d243b264d92f25b5e565e314d36ca2926b8d27
Miguel <mlopez@dreaming-arts.com>
2017-03-02 18:27:26 +0100

    camera resizer

---
+++ Assets/_AmazingFusion/Tools/Scripts/Camera/OrtographicCameraResizer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.AmazingFusion {
+    public class OrtographicCameraResizer : MonoBehaviour {
+
+        [SerializeField]
+        float _referenceWidth;
+
+        [SerializeField]
+        float _referenceHeight;
+
+        [SerializeField]
+        float _match;
+
+        // Use this for initialization
+        void Start() {
+            SetCamera();
+
+#if !UNITY_EDITOR
+                Destroy(this);
+#endif
+
+        }
+
+#if UNITY_EDITOR
+        void Update() {
+            SetCamera();
+        }
+#endif
+
+        void SetCamera() {
+            float ratio = (_referenceWidth / _referenceHeight) / Camera.main.aspect;
+
+            Camera.main.orthographicSize = _referenceHeight * (ratio + (1 - ratio) * _match);
+        }
+    }
+}
