fd1165910be5174120b1de66525cf7a3ecdf48ac
Diego Diaz Perez <diduscdm@gmail.com>
2017-03-03 17:21:13 +0100

    MenuScene

---
+++ Assets/_AmazingFusion/_HyperWhiteBloodCell/Scripts/PlayController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.AmazingFusion.HyperWhiteBloodCell
+{
+    public class PlayController : OptimizedBehaviour
+    {
+        public void PlayClick()
+        {
+            ScenesManager.Instance.LoadScene(ScenesManager.Scene.GameScene);
+        }
+
+    }
+}
+
+
