11b5c1cff146489d634ca9f51cd6db4ee4ca27ee
Miguel <mlopez@dreaming-arts.com>
2017-03-10 12:27:56 +0100

    - loading Scene - on application pause, pause the game - enemies over the walls

--- Assets/_AmazingFusion/_HyperWhiteBloodCell/Scripts/Characters/Spawns/SpawnOnStart.cs
+++ Assets/_AmazingFusion/_HyperWhiteBloodCell/Scripts/Characters/Spawns/SpawnOnStart.cs
@@ -10,7 +10,9 @@ namespace com.AmazingFusion.HyperWhiteBloodCell {
 
         // Use this for initialization
         void Start() {
-            Transform go = Instantiate(_prefab, Transform.position, Transform.rotation);
+            Transform go = Instantiate(_prefab,
+                    new Vector3(Transform.position.x, Transform.position.y, -3f),
+                    Transform.rotation);
             go.SetParent(Transform.parent, true);
             Destroy(gameObject);
         }
---
+++ Assets/_AmazingFusion/_HyperWhiteBloodCell/Scripts/View/ChangeTextAnimation.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MovementEffects;
+using TMPro;
+using System;
+
+namespace com.AmazingFusion.HyperWhiteBloodCell {
+    [RequireComponent(typeof(TMP_Text))]
+    public class ChangeTextAnimation : OptimizedBehaviour, IFixedTickable {
+
+        [SerializeField]
+        string[] _texts;
+
+        [SerializeField]
+        float _rate;
+
+        TMP_Text _text;
+
+        float _nextChange;
+        int _currentIndex;
+
+        void OnDestroy() {
+            if (FixedUpdateManager.HasInstance) {
+                FixedUpdateManager.Instance.Remove(this);
+            }
+        }
+
+        void Awake() {
+            _text = GetComponent<TMP_Text>();
+
+            FixedUpdateManager.Instance.Add(this);
+        }
+
+        public void FixedTick(float realDeltaTime) {
+            if (Time.time > _nextChange) {
+                ++_currentIndex;
+                if (_currentIndex >= _texts.Length) {
+                    _currentIndex = 0;
+                }
+
+                _text.text = _texts[_currentIndex];
+
+                _nextChange = Time.time + _rate;
+            }
+        }
+    }
+}
